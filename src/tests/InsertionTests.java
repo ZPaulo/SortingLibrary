4a8ff9e5e5c1c583a750b9f4c2bd1743a2e28845
Leonardo Faria <ei12072@fe.up.pt>
2016-10-17 16:22:56 +0100

    Insertion tested

--- src/tests/InsertionTests.java
+++ src/tests/InsertionTests.java
@@ -19,10 +19,20 @@ public class InsertionTests extends SortTester {
 		int a1[] = { 5, 4, 3, 2, 1 };
 		int a2[] = a1.clone();
 		InsertionSort.sort(a1);
-		
 		assertTrue(ordered(a1));
 		assertTrue(sameArray(a1, a2));
 
+		int a3[] = {};
+		int a4[]=a3.clone();
+		InsertionSort.sort(a3);
+		assertTrue(ordered(a3));
+		assertTrue(sameArray(a3, a4));
+		
+		int a5[] = {1,2,3,4,5};
+		int a6[] = a5.clone();
+		InsertionSort.sort(a5);
+		assertTrue(ordered(a5));
+		assertTrue(sameArray(a5, a6));
 	}
 
 }
