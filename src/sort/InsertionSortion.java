13e75aa45c0be317f0b935b9120a975e42849913
Leonardo Faria <ei12072@fe.up.pt>
2016-10-17 14:53:10 +0100

    insertion implemented, on to the tests

--- src/sort/InsertionSortion.java
+++ src/sort/InsertionSortion.java
@@ -1,5 +1,14 @@
 package sort;
 
 public class InsertionSortion {
-
+	public static void sort(int[] a){
+		for (int i = 0; i < a.length; i++) {
+			int x = a[i];
+			int j;
+			for (j = i-1; j>=0 && a[j] > x; j--) {
+				a[j+1]=a[j];
+			}
+			a[j+1]=x;
+		}
+	}
 }
