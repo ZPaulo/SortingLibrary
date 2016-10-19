37c77d26756e5e56cfe3a08ef5c79a1c141fa3ef
Leonardo Faria <ei12072@fe.up.pt>
2016-10-19 16:23:00 +0100

    insertion documentation
    missing class docs

--- src/sort/Insertion.java
+++ src/sort/Insertion.java
@@ -1,14 +1,61 @@
 package sort;
 
+/**
+ * 
+ *
+ */
 public abstract class Insertion {
-	public static void sort(int[] a) {
-		for (int i = 1; i < a.length; i++) {
-			int j=i;
-			while (j>0 && a[j-1]> a[j]) {
-				int temp = a[j];
-				a[j]=a[j-1];
-				a[--j]=temp;
+	/**
+	 * Sorts an array using the insertion sort algorithm. Uses
+	 * {@link sort(int[],boolean) sort(array,positions)} by setting
+	 * {@code positions} as {@code false}
+	 * 
+	 * @see sort(int[],boolean)
+	 * @param array
+	 *            array of integers to be sorted
+	 */
+	public static void sort(int[] array) {
+		sort(array, false);
+	}
+
+	/**
+	 * Sorts an array using the merge sort algorithm. Returns the original
+	 * positions of the elements in the sorted array if {@code positions} is set
+	 * as {@code true}.
+	 * 
+	 * @param array
+	 *            array of integers to be sorted
+	 * @param positions
+	 *            if {@code true}, the method returns the original positions of
+	 *            the elements in the sorted array, otherwise returns
+	 *            {@code null}
+	 * @return if {@code positions} is set as {@code true} returns the original
+	 *         positions of the elements in the sorted array, otherwise returns
+	 *         {@code null}
+	 */
+	public static int[] sort(int[] array, boolean positions) {
+		int[] p = null;
+		int i = 0;
+		if (positions) {
+			p = new int[array.length];
+			for (i = 0; i < p.length; i++)
+				p[i] = i;
+		}
+		for (i = 1; i < array.length; i++) {
+			int j = i;
+			while (j > 0 && array[j - 1] > array[j]) {
+				int temp = array[j];
+				array[j] = array[j - 1];
+				array[j] = temp;
+				if (positions) {
+					int ptemp = p[j];
+					p[j] = p[j - 1];
+					p[--j] = ptemp;
+				}
 			}
 		}
+		return p;
+
 	}
+
 }
