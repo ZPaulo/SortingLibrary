5f393d76f31a4e39813c64fdc63c6b75fb36c182
José Oliveira <jose.p.s.olive@gmail.com>
2016-10-19 16:49:44 +0100

    ta

--- src/tests/MergeTests.java
+++ src/tests/MergeTests.java
@@ -12,12 +12,36 @@ public class MergeTests extends SortTester {
 	 * Test method for {@link sort.Merge#sort(int[])}.
 	 */
 	@Test
-	public void testSort() {
+	public void testSortIntArray() {
+		int a1[] = { 5,5, 4, 3, 2, 1 };
+		int a2[] = a1.clone();
+		Merge.sort(a1);
+		
+		assertTrue(ordered(a1));
+		assertTrue(sameArray(a1, a2));
+		
+		int a3[] = {1, 2, 3, 4 ,5,5};
+		Merge.sort(a3);
+		
+		assertTrue(ordered(a3));
+		assertTrue(sameArray(a1, a3));
+		
+		int a4[] = {};
+		int a5[] = a4.clone();
+		Merge.sort(a4);
+		
+		assertTrue(ordered(a4));
+		assertTrue(sameArray(a4, a5));
+	}
+	
+	/**
+	 * Test method for {@link sort.Merge#sort(int[], boolean)}.
+	 */
+	@Test
+	public void testSortIntArrayBoolean() {
 		int a1[] = { 5,5, 4, 3, 2, 1 };
 		int a2[] = a1.clone();
 		int a6[] = Merge.sort(a1, true);
-		for(int i : a6)
-			System.out.println(i);
 		
 		assertTrue(ordered(a1));
 		assertTrue(sameArray(a1, a2));
--- src/tests/SortTests.java
+++ src/tests/SortTests.java
@@ -13,7 +13,7 @@ public class SortTests extends SortTester {
 	 * Test method for {@link sort.Sort#sort(int[])}.
 	 */
 	@Test
-	public void testSort() {
+	public void testSortIntArray() {
 		int a1[] = { 5, 4, 3, 2, 1 };
 		int a2[] = a1.clone();
 		Sort.sort(a1);
@@ -34,6 +34,38 @@ public class SortTests extends SortTester {
 		
 		assertTrue(ordered(a5));
 		assertTrue(sameArray(a6, a5));
+	}
+	
+	/**
+	 * Test method for {@link sort.Sort#sort(int[],boolean)}.
+	 */
+	@Test
+	public void testSortIntArrayBoolean() {
+		int a1[] = { 5, 4, 3, 2, 1 };
+		int a2[] = a1.clone();
+		int a7[] = 	Sort.sort(a1,true);
+	
+		
+		assertTrue(ordered(a1));
+		assertTrue(sameArray(a1, a2));
+		assertTrue(positions(a2, a1, a7));
+		
+		int a3[] = {1, 2, 3, 4 ,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
+		int a4[] = a3.clone();
+		int a8[] = Sort.sort(a3, true);
+		
+		
+		assertTrue(ordered(a3));
+		assertTrue(sameArray(a4, a3));
+		assertTrue(positions(a4, a3, a8));
+		
+		int a5[] = {};
+		int a6[] = a5.clone();
+		int a9[] = Merge.sort(a5,true);
+		
+		assertTrue(ordered(a5));
+		assertTrue(sameArray(a6, a5));
+		assertTrue(positions(a6, a5, a9));
 	}
 
 }
