32cec2a8146a60f7ab80f13df4fbbe6fa8feceb1
Leonardo Faria <ei12072@fe.up.pt>
2016-10-20 14:09:58 +0100

    Refactoring tests

--- src/tests/InsertionTests.java
+++ src/tests/InsertionTests.java
@@ -38,28 +38,36 @@ public class InsertionTests extends SortTester {
 	 */
 	@Test
 	public void testSortIntArrayBoolean() {
-		int temp[];
-		int a1[] = { 5, 4, 3, 2, 1 };
-		int a2[] = a1.clone();
-//		
-		Insertion.sort(a1,true);
-		
-		temp=a1.clone();
-//		assertTrue(Insertion.sort(temp, false)==null);
-		assertTrue(ordered(a1));
-		assertTrue(sameArray(a1, a2));
+		int original[] = { 5, 4, 3, 2, 1 };
+		int sorted[] = original.clone();
+		int temp[] = original.clone();
+		assertTrue(Insertion.sort(temp, false) == null);
+		assertTrue(ordered(temp));
+		assertTrue(sameArray(temp, original));
+		int positionsArray[] = Insertion.sort(sorted, true);
+		assertTrue(ordered(sorted));
+		assertTrue(sameArray(sorted, original));
+		assertTrue(positions(original, sorted, positionsArray));
 
-		int a3[] = {};
-		int a4[] = a3.clone();
-		Insertion.sort(a3);
-		assertTrue(ordered(a3));
-		assertTrue(sameArray(a3, a4));
+		original = new int[] {};
+		sorted = original.clone();
+		temp = original.clone();
+		assertTrue(Insertion.sort(temp, false) == null);
+		positionsArray = Insertion.sort(sorted, true);
+		assertTrue(ordered(sorted));
+		assertTrue(sameArray(sorted, original));
+		assertTrue(positions(original, sorted, positionsArray));
 
-		int a5[] = { 1, 2, 3, 4, 5 };
-		int a6[] = a5.clone();
-		Insertion.sort(a5);
-		assertTrue(ordered(a5));
-		assertTrue(sameArray(a5, a6));
+		original = new int[] { 1, 2, 3, 4, 5 };
+		sorted = original.clone();
+		temp = original.clone();
+		assertTrue(Insertion.sort(temp, false) == null);
+		assertTrue(ordered(temp));
+		assertTrue(sameArray(temp, original));
+		positionsArray = Insertion.sort(sorted, true);
+		assertTrue(ordered(sorted));
+		assertTrue(sameArray(sorted, original));
+		assertTrue(positions(original, sorted, positionsArray));
 	}
 
 }
