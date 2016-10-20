9aff52f58b6f938bc960799d9af7141890aa267e
Leonardo Faria <ei12072@fe.up.pt>
2016-10-20 14:21:08 +0100

    The best refactoring

--- src/tests/InsertionTests.java
+++ src/tests/InsertionTests.java
@@ -9,41 +9,32 @@ import sort.Insertion;
 
 public class InsertionTests extends SortTester {
 
+	void testSorting(int[] original, boolean positions) {
+		int sorted[] = original.clone();
+		int temp[] = original.clone();
+		if (positions)
+			assertTrue(Insertion.sort(temp, false) == null);
+		else
+			Insertion.sort(temp);
+		assertTrue(ordered(temp));
+		assertTrue(sameArray(temp, original));
+		if (positions) {
+			int positionsArray[] = Insertion.sort(sorted, true);
+			assertTrue(ordered(sorted));
+			assertTrue(sameArray(sorted, original));
+			assertTrue(positions(original, sorted, positionsArray));
+		}
+	}
+
 	/**
 	 * Test method for {@link sort.Insertion#sort(int[])}.
 	 */
 	@Test
 	public void testSortIntArray() {
-		int a1[] = { 5, 4, 3, 2, 1 };
-		int a2[] = a1.clone();
-		Insertion.sort(a1);
-		assertTrue(ordered(a1));
-		assertTrue(sameArray(a1, a2));
-
-		int a3[] = {};
-		int a4[] = a3.clone();
-		Insertion.sort(a3);
-		assertTrue(ordered(a3));
-		assertTrue(sameArray(a3, a4));
-
-		int a5[] = { 1, 2, 3, 4, 5 };
-		int a6[] = a5.clone();
-		Insertion.sort(a5);
-		assertTrue(ordered(a5));
-		assertTrue(sameArray(a5, a6));
-	}
-
-	void testSorting(int[] original) {
-		int sorted[] = original.clone();
-		int temp[] = original.clone();
-		assertTrue(Insertion.sort(temp, false) == null);
-		assertTrue(ordered(temp));
-		assertTrue(sameArray(temp, original));
-		int positionsArray[] = Insertion.sort(sorted, true);
-		assertTrue(ordered(sorted));
-		assertTrue(sameArray(sorted, original));
-		assertTrue(positions(original, sorted, positionsArray));
-
+		testSorting(new int[] { 5, 4, 3, 2, 1 }, false);
+		testSorting(new int[] {}, false);
+		testSorting(new int[] { 1, 2, 3, 4, 5 }, false);
+		testSorting(new int[] { 1, 2, 3, 4, 4, 5 }, false);
 	}
 
 	/**
@@ -51,10 +42,10 @@ public class InsertionTests extends SortTester {
 	 */
 	@Test
 	public void testSortIntArrayBoolean() {
-		testSorting(new int[] { 5, 4, 3, 2, 1 });
-		testSorting(new int[] {});
-		testSorting(new int[] { 1, 2, 3, 4, 5 });
-		testSorting(new int[] { 1, 2, 3, 4, 4, 5 });
+		testSorting(new int[] { 5, 4, 3, 2, 1 }, true);
+		testSorting(new int[] {}, true);
+		testSorting(new int[] { 1, 2, 3, 4, 5 }, true);
+		testSorting(new int[] { 1, 2, 3, 4, 4, 5 }, true);
 	}
 
 }
