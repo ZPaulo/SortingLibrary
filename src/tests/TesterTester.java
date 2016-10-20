a8106cb04f041241630de25089c92afc26581125
José Oliveira <jose.p.s.olive@gmail.com>
2016-10-20 14:58:04 +0100

    remove imports

--- src/tests/TesterTester.java
+++ src/tests/TesterTester.java
@@ -1,9 +1,9 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.omg.PortableServer.POAPackage.ObjectAlreadyActive;
 
 public class TesterTester {
 
