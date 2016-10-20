5d9dac2f9d17280c7ce9a7952a077e92271f6395
José Oliveira <jose.p.s.olive@gmail.com>
2016-10-20 14:40:54 +0100

    refactor

--- src/tests/MergeTests.java
+++ src/tests/MergeTests.java
@@ -8,62 +8,44 @@ import sort.Merge;
 
 public class MergeTests extends SortTester {
 
+	
+	void testSorting(int[] original, boolean positions) {
+		int sorted[] = original.clone();
+		int temp[] = original.clone();
+		if (positions)
+			assertTrue(Merge.sort(temp, false) == null);
+		else
+			Merge.sort(temp);
+		assertTrue(ordered(temp));
+		assertTrue(sameArray(temp, original));
+		if (positions) {
+			int positionsArray[] = Merge.sort(sorted, true);
+			assertTrue(ordered(sorted));
+			assertTrue(sameArray(sorted, original));
+			assertTrue(positions(original, sorted, positionsArray));
+		}
+	}
+
 	/**
 	 * Test method for {@link sort.Merge#sort(int[])}.
 	 */
 	@Test
 	public void testSortIntArray() {
-		int a1[] = { 5,5, 4, 3, 2, 1 };
-		int a2[] = a1.clone();
-		Merge.sort(a1);
-		
-		assertTrue(ordered(a1));
-		assertTrue(sameArray(a1, a2));
-		
-		int a3[] = {1, 2, 3, 4 ,5,5};
-		Merge.sort(a3);
-		
-		assertTrue(ordered(a3));
-		assertTrue(sameArray(a1, a3));
-		
-		int a4[] = {};
-		int a5[] = a4.clone();
-		Merge.sort(a4);
-		
-		assertTrue(ordered(a4));
-		assertTrue(sameArray(a4, a5));
+		testSorting(new int[] { 5, 4, 3, 2, 1 }, false);
+		testSorting(new int[] {}, false);
+		testSorting(new int[] { 1, 2, 3, 4, 5 }, false);
+		testSorting(new int[] { 1, 2, 3, 4, 4, 5 }, false);
 	}
-	
+
 	/**
-	 * Test method for {@link sort.Merge#sort(int[], boolean)}.
+	 * Test method for {@link sort.Merge#sort(int[],boolean)}.
 	 */
 	@Test
 	public void testSortIntArrayBoolean() {
-		int a1[] = { 5,5, 4, 3, 2, 1 };
-		int a2[] = a1.clone();
-		int a9[] = a1.clone();
-		int a6[] = Merge.sort(a1, true);
-		
-		
-		assertTrue(ordered(a1));
-		assertTrue(sameArray(a1, a2));
-		assertTrue(positions(a2, a1, a6));
-		assertTrue(Merge.sort(a9,false) == null);
-		
-		int a3[] = {1, 2, 3, 4 ,5,5};
-		int a7[] = Merge.sort(a3, true);
-		
-		assertTrue(ordered(a3));
-		assertTrue(sameArray(a1, a3));
-		assertTrue(positions(a3, a1, a7));
-		
-		int a4[] = {};
-		int a5[] = a4.clone();
-		int a8[] = Merge.sort(a4,true);
-		
-		assertTrue(ordered(a4));
-		assertTrue(sameArray(a4, a5));
-		assertTrue(positions(a4, a5, a8));
+		testSorting(new int[] { 5, 4, 3, 2, 1 }, true);
+		testSorting(new int[] {}, true);
+		testSorting(new int[] { 1, 2, 3, 4, 5 }, true);
+		testSorting(new int[] { 1, 2, 3, 4, 4, 5 }, true);
 	}
 
 }
--- src/tests/SortTests.java
+++ src/tests/SortTests.java
@@ -4,70 +4,49 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import sort.Merge;
 import sort.Sort;
 
 public class SortTests extends SortTester {
+
+	void testSorting(int[] original, boolean positions) {
+		int sorted[] = original.clone();
+		int temp[] = original.clone();
+		if (positions)
+			assertTrue(Sort.sort(temp, false) == null);
+		else
+			Sort.sort(temp);
+		assertTrue(ordered(temp));
+		assertTrue(sameArray(temp, original));
+		if (positions) {
+			int positionsArray[] = Sort.sort(sorted, true);
+			assertTrue(ordered(sorted));
+			assertTrue(sameArray(sorted, original));
+			assertTrue(positions(original, sorted, positionsArray));
+		}
+	}
 
 	/**
 	 * Test method for {@link sort.Sort#sort(int[])}.
 	 */
 	@Test
 	public void testSortIntArray() {
-		int a1[] = { 5, 4, 3, 2, 1 };
-		int a2[] = a1.clone();
-		Sort.sort(a1);
-		
-		assertTrue(ordered(a1));
-		assertTrue(sameArray(a1, a2));
-		
-		int a3[] = {1, 2, 3, 4 ,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
-		int a4[] = a3.clone();
-		Sort.sort(a3);
-		
-		assertTrue(ordered(a3));
-		assertTrue(sameArray(a4, a3));
-		
-		int a5[] = {};
-		int a6[] = a5.clone();
-		Merge.sort(a5);
-		
-		assertTrue(ordered(a5));
-		assertTrue(sameArray(a6, a5));
+		testSorting(new int[] { 5, 4, 3, 2, 1 }, false);
+		testSorting(new int[] { 5, 4, 3, 2, 1,2,3,4,5,7,7}, false);
+		testSorting(new int[] {}, false);
+		testSorting(new int[] { 1, 2, 3, 4, 5 }, false);
+		testSorting(new int[] { 1, 2, 3, 4, 4, 5 }, false);
 	}
-	
+
 	/**
 	 * Test method for {@link sort.Sort#sort(int[],boolean)}.
 	 */
 	@Test
 	public void testSortIntArrayBoolean() {
-		int a1[] = { 5, 4, 3, 2, 1 };
-		int a2[] = a1.clone();
-		int a7[] = 	Sort.sort(a1,true);
-	
-		
-		assertTrue(ordered(a1));
-		assertTrue(sameArray(a1, a2));
-		assertTrue(positions(a2, a1, a7));
-		
-		int a3[] = {1, 2, 3, 4 ,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
-		int a4[] = a3.clone();
-		int a10[] = a3.clone();
-		int a8[] = Sort.sort(a3, true);
-		
-		
-		assertTrue(ordered(a3));
-		assertTrue(sameArray(a4, a3));
-		assertTrue(positions(a4, a3, a8));
-		assertTrue(Merge.sort(a10,false) == null);
-		
-		int a5[] = {};
-		int a6[] = a5.clone();
-		int a9[] = Merge.sort(a5,true);
-		
-		assertTrue(ordered(a5));
-		assertTrue(sameArray(a6, a5));
-		assertTrue(positions(a6, a5, a9));
+		testSorting(new int[] { 5, 4, 3, 2, 1 }, true);
+		testSorting(new int[] { 5, 4, 3, 2, 1,2,3,4,5,7,7}, true);
+		testSorting(new int[] {}, true);
+		testSorting(new int[] { 1, 2, 3, 4, 5 }, true);
+		testSorting(new int[] { 1, 2, 3, 4, 4, 5 }, true);
 	}
 
 }
